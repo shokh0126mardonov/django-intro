7cb4a9563d71e6968ac4a31c0d0548508aefddc3
shokh0126mardonov <shokh0126mardonov@example.com>
2025-10-29 18:09:03 +0500

    birinchi django proektim

---
+++ pages/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# home_wiew,home_about,home_contact,home_login,home_register,home_profile
+
+def home_wiew(x):
+    return HttpResponse(
+        'home page'
+    )
+
+def home_about(x):
+    return HttpResponse(
+        'home page'
+    )
+
+def home_contact(x):
+    return HttpResponse(
+        'home page'
+    )
+
+def home_login(x):
+    return HttpResponse(
+        'home page'
+    )
+
+def home_register(x):
+    return HttpResponse(
+        'home page'
+    )
+
+def home_profile(x):
+    return HttpResponse(
+        'home page'
+    )
+
